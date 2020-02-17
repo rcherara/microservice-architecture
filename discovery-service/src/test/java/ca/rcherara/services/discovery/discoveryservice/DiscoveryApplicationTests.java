d8d25ef3dfa7b4f9760d1b0811d883a15a1f95a4
REDDAH CHERARA <rcherara@rcherara-macbook.local>
2020-02-17 06:58:40 -0500

    Commit Discovery Server

---
+++ discovery-service/src/test/java/ca/rcherara/services/discovery/discoveryservice/DiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package ca.rcherara.services.discovery.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
