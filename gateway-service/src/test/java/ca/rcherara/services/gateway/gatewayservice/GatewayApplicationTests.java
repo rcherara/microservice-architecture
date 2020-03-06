0af8d5f3bfe90d78e606d302fd69abc6fe3bc21d
CHERARA REDDAH <cherara.reddah@icloud.com>
2020-03-06 00:02:32 -0500

    Add gateway-service

---
+++ gateway-service/src/test/java/ca/rcherara/services/gateway/gatewayservice/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.rcherara.services.gateway.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
